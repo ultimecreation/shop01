763727605c39d3b108abf77a2d7632c8cc337724
ultimecreation <naserbechata@gmail.com>
2024-08-01 08:10:33 +0200

    start

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    plugins: ["jest-dom"],
+    extends: [
+        "react-app",
+        "react-app/jest",
+        "plugin:jest-dom/recommended",
+    ]
+
+};
--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [state, setState] = useState()
   return (
     <div className="App">
       <header className="App-header">
